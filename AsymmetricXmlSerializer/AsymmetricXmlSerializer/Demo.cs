4332fe7bb2662841766eabfaccc681e48757c754
Paul Evans <paul.d.evans+paulecoyote@gmail.com>
2011-06-10 20:09:04 -0500

    Cleaned up a little bit (assert should have been at the bottom etc). Added notes at the bottom of file. Improved comments at the top.

--- AsymmetricXmlSerializer/AsymmetricXmlSerializer/Demo.cs
+++ AsymmetricXmlSerializer/AsymmetricXmlSerializer/Demo.cs
@@ -1,7 +1,8 @@
 ﻿// See Clue class to change demo behaviour.  
-// Define / search for FIX1 and FIX2
+// Uncomment one of the fixes below to observe different behaviours.
+// Search for FIX1 or FIX2 to see where the changes have been made.
 //// #define FIX1  // Do *not* use DefaultValue attribute, taken in to account serialising but not de-serialising.
-//// #define FIX2  // Set default value of variable to same as DefaultValue attribute.
+//// #define FIX2  // Set default value of variable to same as DefaultValue attribute during construction.
 
 namespace AsymmetricXmlSerializer
 {
@@ -16,7 +17,7 @@ namespace AsymmetricXmlSerializer
     /// <summary>
     /// Class to be serialized and demo point for asymmetric XmlSerializer behaviour.
     /// </summary>
-    [Serializable(), XmlRoot(Namespace = "")]
+    [Serializable()]
     public class Clue
     {
         /// <summary>
@@ -47,7 +48,7 @@ namespace AsymmetricXmlSerializer
 
             // Buffer for writing.
             StringBuilder buffer = new StringBuilder();
-            
+
             // Don't need namespaces.
             XmlSerializerNamespaces no_namespaces = new XmlSerializerNamespaces();
             no_namespaces.Add(string.Empty, string.Empty);
@@ -57,41 +58,59 @@ namespace AsymmetricXmlSerializer
 
             // Serialise the clue...
             XmlSerializer xml_serializer = new XmlSerializer(typeof(Clue));
-            using (StringWriter string_writer = new StringWriter(buffer))
+            StringWriter string_writer = new StringWriter(buffer);
+            using (XmlWriter xml_writer = XmlWriter.Create(string_writer, writer_settings))
             {
-                using (XmlWriter xml_writer = XmlWriter.Create(string_writer, writer_settings))
-                {
-                    xml_serializer.Serialize(xml_writer, clue, no_namespaces);
-                }
+                xml_serializer.Serialize(xml_writer, clue, no_namespaces);
             }
 
-            string serialised = buffer.ToString();
+            string serialized = buffer.ToString();
 
             // Show serialised version of the clue...
             Console.WriteLine("Clue text value: {0}", clue.text);
-            Console.WriteLine("Clue persisted:  {0}", buffer.ToString());
+            Console.WriteLine("Clue serialized: {0}", serialized);
 
             // Deserialise the clue...
             Clue clue_read;
-            using (StringReader sr = new StringReader(serialised))
+            using (StringReader sr = new StringReader(serialized))
             {
                 clue_read = (Clue)xml_serializer.Deserialize(sr);
             }
 
-            Debug.Assert(clue.text == clue_read.text, "Written & read clue's text should match");
-
             // Show clue 2 on console
             // Reset and reuse write buffer.
             buffer.Length = 0;
-            using (StringWriter string_writer = new StringWriter(buffer))
+            using (XmlWriter xml_writer = XmlWriter.Create(string_writer, writer_settings))
             {
-                using (XmlWriter xml_writer = XmlWriter.Create(string_writer, writer_settings))
-                {
-                    xml_serializer.Serialize(xml_writer, clue_read, no_namespaces);
-                    Console.WriteLine("Read Clue text value: {0}", clue_read.text);
-                    Console.WriteLine("Read Clue: {0}", buffer.ToString());
-                }
+                xml_serializer.Serialize(xml_writer, clue_read, no_namespaces);
             }
+
+            string reserialized = buffer.ToString();
+            Console.WriteLine("Read Clue text value: {0}", clue_read.text);
+            Console.WriteLine("Read Clue serialized: {0}", reserialized);
+
+            Debug.Assert(clue.text == clue_read.text, "Written & read clue's text should match");
         }
     }
 }
+
+/* NOTES
+ * FIX1: Don't use DefaultValueAttribute.
+ * 
+ * You would want to use DefaultValueAttribute to avoid persisting the
+ * default value to the xml representation.
+ * Although this does prevent the default value being persisted, on read the
+ * DefaultValueAttribute is ignored so the value is never set to that of
+ * the DefaultValueAttribute.
+ * DefaultValueAttribute is also used for value hints for UI.
+ * See: http://msdn.microsoft.com/en-us/library/2cws2s9d.aspx
+ * 
+ * 
+ * FIX2: Use DefaultValueAttribute AND default value at construction.
+ * 
+ * To use DefaultValueAttribute and have the object be recreated 
+ * properly when reading it back, you must to explictly set the 
+ * default value during construction of the object being read.
+ * Using this method means the xml is concise and the object
+ * is accurately recreated.
+ */
